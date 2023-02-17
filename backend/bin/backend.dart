4f2cd85525452fcbae45ffd760f570e2b8036c8e
Alexandre Mathias <mathias.alexandre@gmail.com>
2023-02-17 22:23:23 +0100

    Updated Backend

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,5 +1,6 @@
-import 'package:backend/backend.dart' as backend;
 import 'dart:io';
+
+import 'package:backend/backend.dart' as backend;
 
 void main(List<String> arguments) {
   print('Hello world: ${backend.calculate()}!');
